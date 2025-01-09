160a1dd4543a1e313f75af12fa4d6059709b1559
sameom1048 <samvision99@naver.com>
2025-01-09 15:26:35 +0900

    add RepositoryProvider

---
+++ src/main/java/app/domain/wiseSaying/repository/RepositoryProvider.java
@@ -0,0 +1,17 @@
+package app.domain.wiseSaying.repository;
+
+import app.global.AppConfig;
+
+public class RepositoryProvider {
+
+    public static WiseSayingRepository provide() {
+//        if(AppConfig.isFileDb()) {
+//            return new WiseSayingFileRepository();
+//        }
+//        else {
+//            return new WiseSayingMemRepository();
+//        }
+
+        return new WiseSayingFileRepository();
+    }
+}
