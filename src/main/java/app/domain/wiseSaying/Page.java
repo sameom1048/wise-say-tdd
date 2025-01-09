3d2af640f9ff6088e20e41f011baef539c8d8776
sameom1048 <samvision99@naver.com>
2025-01-09 15:26:25 +0900

    add Member & Page

---
+++ src/main/java/app/domain/wiseSaying/Page.java
@@ -0,0 +1,22 @@
+package app.domain.wiseSaying;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class Page<T> {
+
+    @Getter
+    private List<T> content;
+    @Getter
+    private int totalItems;
+    private int itemsPerPage;
+    @Getter
+    private int page;
+
+    public int getTotalPages() {
+        return (int) Math.ceil((double) totalItems / itemsPerPage);
+    }
+}
